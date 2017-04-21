b2a527178d8a852453a3dbf2c4dd6936dbd22855
wack <kai.wang@citrix.com>
2017-04-21 12:22:05 +0800

    test

---
+++ 238. Product of Array Except Self.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    	int n = nums.size();
+        vector<int> a(n,1);
+        vector<int> b(n,1);
+        vector<int> c(n,1);
+        for(int i=1;i<n;i++){
+        		a[i] = a[i-1] * nums[i-1];
+        		b[i] = b[i-1] * nums[n-i];
+        }
+        for(int i=0;i<n;i++)
+        	c[i] = a[i]*b[n-i-1];
+        return c;
+    }
+};
