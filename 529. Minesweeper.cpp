0e2868679efca8e6ca83e9b4bbb93021e32365e6
wack <kai.wang@citrix.com>
2017-04-24 12:04:20 +0800

    529

--- 529. Minesweeper.cpp
+++ 529. Minesweeper.cpp
@@ -36,24 +36,33 @@ Output:
     int hasM(vector<vector<char>>& board,int x,int y,int r,int c){
         int cnt=0;
         
-        if(y-1>=0 && boadr[x][y-1]=='M') cnt++;
-        if(y+1<c  && boadr[x][y+1]=='M') cnt++;
+        if(y-1>=0 && board[x][y-1]=='M') cnt++;
+        if(y+1<c  && board[x][y+1]=='M') cnt++;
 
-        if(x-1>=0 && y-1>=0 && boadr[x-1][y-1]=='M') cnt++;
-        if(x-1>=0 && boadr[x-1][y]=='M') cnt++;
-        if(x-1>=0 && y+1<c && boadr[x-1][y+1]=='M') cnt++;
-        
-        if(x+1<r && boadr[x+1][y]=='M') cnt++;
-        if(x+1<r && y-1>=0 && boadr[x+1][y-1]=='M') cnt++;
-        if(x+1<r && y+1<c && boadr[x+1][y+1]=='M') cnt++;
+        if(x-1>=0 && y-1>=0 && board[x-1][y-1]=='M') cnt++;
+        if(x-1>=0 && board[x-1][y]=='M') cnt++;
+        if(x-1>=0 && y+1<c && board[x-1][y+1]=='M') cnt++;
+
+        if(x+1<r && board[x+1][y]=='M') cnt++;
+        if(x+1<r && y-1>=0 && board[x+1][y-1]=='M') cnt++;
+        if(x+1<r && y+1<c && board[x+1][y+1]=='M') cnt++;
         return cnt;
     }
     void dfs(vector<vector<char>>& board,int x,int y,int r,int c){
-        int cnt = hasM(board,x,y,r,c);
-        if(cnt){ board[x][y]=cnt+'0'; return ;}
-        else{
-            board[x][y] = 'B';
-            dfs(board)
+        if(board[x][y]=='E'){
+            int cnt = hasM(board,x,y,r,c);  //周围雷的数量
+            if(cnt){ board[x][y]=cnt+'0'; return ;} //周围有雷
+            else{  //周围没有雷 需要dfs周围
+                board[x][y] = 'B';
+                if(x-1>=0 && y-1>=0)dfs(board,x-1,y-1,r,c);
+                if(x-1>=0)dfs(board,x-1,y,r,c);
+                if(x-1>=0 && y+1<c)dfs(board,x-1,y+1,r,c);
+                if(y-1>=0)dfs(board,x,y-1,r,c);
+                if(y+1<c)dfs(board,x,y+1,r,c);
+                if(x+1<r && y-1>=0)dfs(board,x+1,y-1,r,c);
+                if(x+1<r)dfs(board,x+1,y,r,c);
+                if(x+1<r && y+1<c)dfs(board,x+1,y+1,r,c);
+            }
         }
     }
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
